127e48763ee7fda69b7551e1b3a610b765671eb3
ASUS <a78439@alunos.uminho.pt>
2020-09-01 16:25:59 +0100

    teste

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -10,23 +10,27 @@ object Main extends App{
   //val df = readData.df
   if (args.length == 0) {
     println("dude, i need at least one parameter")
-  }else {
-    val df = read(args(0))
+
+  }else if (args.length == 5 && (args(0)=="csv" || args(0) == "txt")) {
+    val df = new readData(conn).readCsv(args)
     val dataAnalysis= new DataAnalysis(conn, df)
     val write = new writeData(df, args)
     dataAnalysis.createAnalysis()
     write.writeAnalysis(dataAnalysis.createAnalysis())
 
   }
-  def read(x: String): DataFrame = x match {
-    case "csv" => val df = new readData(conn).readCsv(args)
-      return df
-    case "txt" => val df = new readData(conn).readCsv(args)
-      return df
-    case "json" => val df = new readData(conn).readJson(args)
-      return df
+  else if (args.length == 4 && args(0) =="json"){
+    val df = new readData(conn).readJson(args)
+    val dataAnalysis= new DataAnalysis(conn, df)
+    val write = new writeData(df, args)
+    dataAnalysis.createAnalysis()
+    write.writeAnalysis(dataAnalysis.createAnalysis())
   }
-
+  else {
+    println("Wrong Input")
+    println("for csv or txt files you need five parameters. Example <file format> <delimiter> <url server> <source path> <destination path>")
+    println("for json files you need four parameters. Example <file format> <url server> <source path> <destination path>")
+  }
 
 }
 
